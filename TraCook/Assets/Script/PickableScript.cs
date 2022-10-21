811d42551244c244f630ad349f365d2971136b3a
lasiest <valenstiowiyogo@gmail.com>
2022-10-21 19:52:59 +0700

    Cut Object(?)

---
+++ TraCook/Assets/Script/PickableScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableScript : MonoBehaviour
+{
+
+        bool pressed = false;
+
+        void OnMouseDown()
+        {
+            pressed = true;
+            GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+
+        void OnMouseUp()
+        {
+            pressed = false;
+            GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (pressed)
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = mousePos;
+            }
+        }
+}
---
+++ TraCook/Assets/Script/SplitableObject.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitableObject : MonoBehaviour
+{
+
+    public bool clicked;
+    public bool move;
+
+    void Start()
+    {
+        GetComponent<PickableScript>().enabled = false;
+        clicked = false;
+        move = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (move == true)
+        {
+            transform.position = (transform.position + new Vector3(-0.001f, 0, 0));
+        }
+
+        /*
+        if (Input.GetMouseButtonDown(0) && !clicked)
+        {
+            float flag = 0f;
+            clicked = true;
+            while(flag <= 10)
+            {
+                transform.position = new Vector3(-1, 0, 0);
+                flag++;
+            }
+        }
+        */
+    }
+    
+    void OnMouseDown()
+    {
+        if (!clicked)
+        {
+            clicked = true;
+            /*
+            double flag = 0f;
+            Debug.Log("Clicked");
+            clicked = true;
+            */
+            StartCoroutine(Wait());
+            /*
+            while (flag <= 10)
+            { 
+                transform.position = transform.position + new Vector3(-1,0,0);
+                Debug.Log(transform.position);
+                flag++;
+            }
+            */
+        }
+    }
+
+    public IEnumerator Wait()
+    {
+        move = true;
+        yield return new WaitForSeconds(1f);
+        move = false;
+        GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
+        GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
+        GetComponent<PickableScript>().enabled = true;
+    }
+
+
+}
